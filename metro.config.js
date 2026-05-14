27d3a25afe84944d1229405163cb1da0b5bcd250
deathztalker <johnpro38@gmail.com>
2026-05-14 00:33:46 -0400

    fix

--- metro.config.js
+++ metro.config.js
@@ -3,5 +3,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
 
 config.resolver.assetExts.push('png', 'jpg', 'jpeg', 'gif', 'svg');
+config.resolver.unstable_enableSymlinks = true;
+config.resolver.unstable_enablePackageExports = true;
 
 module.exports = config;
