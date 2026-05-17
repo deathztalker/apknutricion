c60e746bb239c65cc63e9f8a848daf4c4e214beb
deathztalker <johnpro38@gmail.com>
2026-05-16 22:14:58 -0400

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -4,6 +4,7 @@ const { withNativeWind } = require('nativewind/metro');
 const config = getDefaultConfig(__dirname);
 
 config.resolver.assetExts.push('png', 'jpg', 'jpeg', 'gif', 'svg');
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'mjs', 'cjs'];
 config.resolver.unstable_enableSymlinks = true;
 config.resolver.unstable_enablePackageExports = true;
 
